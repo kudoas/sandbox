c8941f6d1fc3b3849e30d4750d2ce1b77edd2b99
Daichi Kudo <sakanactor624@gmail.com>
2020-02-27 21:44:15 +0900

    :books: Add template engines (handlebar, ejs)

--- dynamic-content/app.js
+++ dynamic-content/app.js
@@ -2,8 +2,25 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+// handlerbar
+const expressHbs = require("express-handlebars");
 
 const app = express();
+
+// handlerbar
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs"
+//   })
+// );
+// app.set("view engine", "handlebars");
+
+// ejs
+// app.set("view engine", "ejs");
+// particleを利用可能(Reactでいうところのコンポーネント的な)
 
 // expressが対応しているtemplate engineなら簡単に導入可能
 app.set("view engine", "pug");
