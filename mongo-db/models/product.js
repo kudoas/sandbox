83dbd4cf4365d86e3147c747813fc1679ee301bf
Daichi Kudo <sakanactor624@gmail.com>
2020-04-09 21:34:04 +0900

    :books: Fetch and Edit products

--- mongo-db/models/product.js
+++ mongo-db/models/product.js
@@ -1,19 +1,56 @@
+const mongodb = require("mongodb");
+
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, imageUrl, price, description, id) {
     this.title = title;
-    this.price = price;
     this.imageUrl = imageUrl;
+    this.price = price;
     this.description = description;
+    this._id = new mongodb.ObjectId(id);
   }
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // update the product
+      dbOp = db.collection("products").updateOne({ _id: this._id }, { $set: this });
+    } else {
+      dbOp = db.collection("products").insertOne(this);
+    }
+    return dbOp
       .then((result) => {
         console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((products) => {
+        console.log("all products", products);
+        return products;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then((product) => {
+        return product;
       })
       .catch((err) => {
         console.log(err);
