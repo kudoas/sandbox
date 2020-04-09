bc5453f1046c3945957769e35023e59818a45299
Daichi Kudo <sakanactor624@gmail.com>
2020-04-09 22:53:31 +0900

    :books: Add User model

--- mongo-db/app.js
+++ mongo-db/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect;
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,13 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById("5e8f247a1c9d4400000b790b")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
 app.use("/admin", adminRoutes);
@@ -33,6 +33,6 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect((client) => {
-  console.log(client);
+  // console.log(client);
   app.listen(3000);
 });
--- mongo-db/models/product.js
+++ mongo-db/models/product.js
@@ -3,12 +3,13 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Product {
-  constructor(title, imageUrl, price, description, id) {
+  constructor(title, imageUrl, price, description, id, userId) {
     this.title = title;
     this.imageUrl = imageUrl;
     this.price = price;
     this.description = description;
-    this._id = new mongodb.ObjectId(id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
+    this.userId = userId;
   }
   save() {
     const db = getDb();
@@ -51,6 +52,19 @@ class Product {
       .next()
       .then((product) => {
         return product;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static deleteById(prodId) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(prodId) })
+      .then((result) => {
+        console.log(result);
       })
       .catch((err) => {
         console.log(err);
