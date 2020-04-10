6c130a78cceeddeb50b4aa786f098d24dfd184f9
Daichi Kudo <sakanactor624@gmail.com>
2020-04-11 01:17:47 +0900

    :sparkles: Add only 1 product to cart

--- mongo-db/app.js
+++ mongo-db/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("5e8f247a1c9d4400000b790b")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
