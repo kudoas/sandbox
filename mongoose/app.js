4b560a917639435be152d0074e60010e4d13b4c9
Daichi Kudo <sakanactor624@gmail.com>
2020-04-14 15:27:02 +0900

    :books: Connect mongodb used by Mongoose

---
+++ mongoose/app.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findById("5baa2528563f16379fc8a610")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(
+    "mongodb+srv://kudoa:usernamekudoa@cluster0-m1vro.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+  .then((result) => {
+    console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
