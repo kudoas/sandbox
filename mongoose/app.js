5c69bddb7d90c3b12054a054b3a4435f525e9c64
Daichi Kudo <sakanactor624@gmail.com>
2020-04-24 08:08:34 +0900

    :books: Connected mongo container

--- mongoose/app.js
+++ mongoose/app.js
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("5baa2528563f16379fc8a610")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById("5baa2528563f16379fc8a610")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.log(err));
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -34,10 +34,13 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://kudoa:usernamekudoa@cluster0-m1vro.mongodb.net/shop?retryWrites=true&w=majority"
+    // "mongodb+srv://kudoa:usernamekudoa@cluster0-m1vro.mongodb.net/shop?retryWrites=true&w=majority"
+    "mongodb://localhost:27017/shop",
+    { auth: { user: "testuser", password: "password" } }
   )
   .then((result) => {
-    console.log(result);
+    // console.log(result);
+    console.log("Connected mongodb!!!");
     app.listen(3000);
   })
   .catch((err) => {
--- mongoose/routes/shop.js
+++ mongoose/routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // router.get('/', shopController.getIndex);
 
-// router.get('/products', shopController.getProducts);
+router.get("/products", shopController.getProducts);
 
 // router.get('/products/:productId', shopController.getProduct);
 
