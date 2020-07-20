1c513a834bf2c8415d9f2c3f61b169822f26de2a
Daichi Kudo <sakanactor624@gmail.com>
2020-07-20 15:12:43 +0900

    :sparkles: first go project

---
+++ go/first-project/hello.go
@@ -0,0 +1,15 @@
+package main
+
+// export PATH=$PATH:$(dirname $(go list -f '{{.Target}}' .))
+
+import (
+	"fmt"
+
+	"example.com/user/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
+)
+
+func main() {
+	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+}
