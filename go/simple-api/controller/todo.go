87b3af9faf08adda0b7983e8cc3f9ff34e4150ea
Daichi Kudo <sakanactor624@gmail.com>
2020-07-29 00:57:39 +0900

    :sparkles: POST, create todo

--- go/simple-api/controller/todo.go
+++ go/simple-api/controller/todo.go
@@ -2,51 +2,72 @@ package controller
 
 import (
 	"log"
-	"strconv"
+	"net/http"
 
-	"github.com/Kudoas/sandbox/go/simple-api/repository"
+	"github.com/Kudoas/sandbox/go/simple-api/model"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
-// func Index(ctx *gin.Context) {
-// 	repository.AllTodo()
+type Todo struct {
+	Text   string `json: "title"`
+	Status string `json: "status"`
+}
+
+func AllTodo(c *gin.Context) {
+	todos := make([]model.Todo, 0)
+	db, err := gorm.Open("sqlite3", "sample-app.sqlite3")
+	if err != nil {
+		log.Print(err)
+	}
+	db.Find(&todos)
+	db.Close()
+	c.JSON(http.StatusOK, gin.H{"data": todos})
+}
+
+func Create(c *gin.Context) {
+	var todo Todo
+	db, err := gorm.Open("sqlite3", "sample-app.sqlite3")
+	if err != nil {
+		log.Print(err)
+	}
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	newTodo := model.Todo{Text: todo.Text, Status: todo.Status}
+	db.Create(&newTodo)
+	c.JSON(http.StatusOK, gin.H{"data": newTodo})
+}
+
+// func Detail(ctx *gin.Context) {
+// 	n := ctx.Param("id")
+// 	id, err := strconv.Atoi(n)
+// 	if err != nil {
+// 		log.Print(err)
+// 	}
+// 	todo := repository.FindTodo(id)
+// 	ctx.HTML(200, "detail.html", gin.H{"todo": todo})
 // }
 
-func Create(ctx *gin.Context) {
-	text := ctx.PostForm("text")
-	status := ctx.PostForm("status")
-	repository.CreateTodo(text, status)
-	ctx.Redirect(302, "/")
-}
+// func Update(ctx *gin.Context) {
+// 	n := ctx.Param("id")
+// 	id, err := strconv.Atoi(n)
+// 	if err != nil {
+// 		log.Print(err)
+// 	}
+// 	text := ctx.PostForm("text")
+// 	status := ctx.PostForm("status")
+// 	repository.UpdateTodo(id, text, status)
+// 	ctx.Redirect(302, "/")
+// }
 
-func Detail(ctx *gin.Context) {
-	n := ctx.Param("id")
-	id, err := strconv.Atoi(n)
-	if err != nil {
-		log.Print(err)
-	}
-	todo := repository.FindTodo(id)
-	ctx.HTML(200, "detail.html", gin.H{"todo": todo})
-}
-
-func Update(ctx *gin.Context) {
-	n := ctx.Param("id")
-	id, err := strconv.Atoi(n)
-	if err != nil {
-		log.Print(err)
-	}
-	text := ctx.PostForm("text")
-	status := ctx.PostForm("status")
-	repository.UpdateTodo(id, text, status)
-	ctx.Redirect(302, "/")
-}
-
-func Delete(ctx *gin.Context) {
-	n := ctx.Param("id")
-	id, err := strconv.Atoi(n)
-	if err != nil {
-		log.Print(err)
-	}
-	repository.DeleteTodo(id)
-	ctx.Redirect(302, "/")
-}
+// func Delete(ctx *gin.Context) {
+// 	n := ctx.Param("id")
+// 	id, err := strconv.Atoi(n)
+// 	if err != nil {
+// 		log.Print(err)
+// 	}
+// 	repository.DeleteTodo(id)
+// 	ctx.Redirect(302, "/")
+// }
--- go/simple-api/main.go
+++ go/simple-api/main.go
@@ -3,8 +3,8 @@
 package main
 
 import (
+	"github.com/Kudoas/sandbox/go/simple-api/controller"
 	"github.com/Kudoas/sandbox/go/simple-api/db"
-	"github.com/Kudoas/sandbox/go/simple-api/repository"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/mattn/go-sqlite3"
@@ -14,8 +14,8 @@ func main() {
 	db.ConnectDB()
 
 	router := gin.Default()
-	router.GET("/todos", repository.AllTodo)
-	// router.POST("/new", controller.Create)
+	router.GET("/todos", controller.AllTodo)
+	router.POST("/new", controller.Create)
 	// router.GET("/:id", controller.Detail)
 	// router.PATCH("/:id", controller.Update)
 	// router.DELETE("/:id", controller.Delete)
