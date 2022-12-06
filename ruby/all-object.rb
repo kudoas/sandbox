c243b7fefacd03be17ebdaf4fa4e510df80a482a
Daichi Kudo <sakanactor624@gmail.com>
2022-12-07 00:14:27 +0900

    rubyのコード補完が効くようにした（ただしRuboCopが暴走中）

--- ruby/all-object.rb
+++ ruby/all-object.rb
@@ -1,2 +1,12 @@
 # https://rurema.clear-code.com/3.2.0/method/Module/i/ancestors.html
-p 'hellp'.class.ancestors
+'hellp'.class.ancestors
+
+def hello(names)
+  names.each do |name|
+    puts "HELLO, #{name.upcase}"
+  end
+end
+
+rubies = %w[MRI jruby mruby]
+
+hello(rubies)
