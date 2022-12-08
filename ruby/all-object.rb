0e622b8aa3dcabb0247ac1b267e358e417ea2640
Daichi Kudo <sakanactor624@gmail.com>
2022-12-09 00:03:09 +0900

    :sparkle: 変数・マジックコメント・レシーバー

--- ruby/all-object.rb
+++ ruby/all-object.rb
@@ -1,3 +1,5 @@
+# coding: euc-jp
+
 # https://rurema.clear-code.com/3.2.0/method/Module/i/ancestors.html
 'hellp'.class.ancestors
 
@@ -7,6 +9,19 @@ def hello(names)
   end
 end
 
-rubies = %w[MRI jruby mruby]
+rubies = ['MRI', 'jruby', 'mruby']
 
 hello(rubies)
+
+File.open 'README.md' do |file|
+  puts file.read
+end
+
+line = 'oneline'; puts(line)
+
+# global変数は$
+$global_num = 777
+
+# 定数は大文字
+ENV = 'production'
+ENV = 'staging'
