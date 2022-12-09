272baadc86796d6f927b99e7cb09b2c242f39ba6
Daichi Kudo <sakanactor624@gmail.com>
2022-12-10 01:05:51 +0900

    :sparkles: クラス内変数の定義

--- ruby/klass.rb
+++ ruby/klass.rb
@@ -11,6 +11,23 @@ my_object.hello
 class Ruler
   attr_accessor :length # length=とlength
 
+  def initialize(length=nil)
+    @length = length
+  end
+
+  # instance method
+  def self.pair
+    [Ruler.new(250), Ruler.new]
+  end
+  
+  def display_length
+    puts length # 参照は省略できる
+  end
+
+  def set_length
+    self.length = 30 # 代入は省略できない
+  end
+  
   # インスタンス変数の代入するときに使うメソッド
   # def length=(val)
   #   @length = val
@@ -22,6 +39,35 @@ class Ruler
   # end
 end
 
-ruler = Ruler.new
+ruler = Ruler.new()
+puts ruler.length
+
 ruler.length = 10 # length= が呼ばれる
 puts ruler.length
+ruler.display_length
+
+ruler.set_length
+puts ruler.length
+
+my_rule = Ruler.new(100)
+puts ruler.length
+
+p Ruler.pair
+
+class MyClass
+  # クラス変数：クラスとインスタンスがスコープ
+  @@cvar = 'Hello, My class variable'
+
+  def cvar_in_method
+    p @@cvar
+  end
+
+  def self.cvar_in_class_method
+    p @@cvar
+  end
+end
+
+my_object = MyClass.new
+
+my_object.cvar_in_method
+MyClass.cvar_in_class_method
