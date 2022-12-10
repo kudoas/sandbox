89df668f497d4ebe127aba30f052d020e53995a4
Daichi Kudo <sakanactor624@gmail.com>
2022-12-10 13:10:06 +0900

    :sparkles: 制御系の構文（while, untile, for）

---
+++ ruby/method1.rb
@@ -0,0 +1,46 @@
+class MyObject
+  def ==(other)
+    self.class == other.class
+  end
+end
+
+MyObject.new == MyObject.new
+
+# 同じオブジェクトかはequal?を使う
+a = [1, 2, 3]
+b = [1, 2, 3]
+a.equal?(b) # => false
+
+n = 0
+p "0!!!" if n.zero?
+
+stone = "ruby" 
+
+# caseは===で比較するのでregexpでも書ける
+case stone
+when "ruby" # stone == "ruby"
+  puts "7"
+when "perl", "white"
+  puts "6"
+else
+  "unknown"
+end
+
+languages = ["Perl", "Python", "Ruby"]
+i = 0
+while i < languages.length # untilは逆
+  p languages[i]
+  i += 1
+end
+
+# sleep 1 while processing?
+# begin
+#   process1
+#   process2
+# end while needed?
+
+# while, until, forはブロックにならない
+for name in ["Alice", "Bob", "Carol"]
+  p name
+end
+p name # => Carol
