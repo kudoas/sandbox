123429a45486a74eab0f848f9c78bc0b141e8b83
Daichi Kudo <sakanactor624@gmail.com>
2022-12-10 11:46:19 +0900

    :sparkles: rubyの基礎done

--- ruby/object2.rb
+++ ruby/object2.rb
@@ -15,3 +15,42 @@ people = %w[Mario Peach Teresa]
 p people[0]
 
 %i(red, green, blue)
+
+# hash
+colors = { red: "ff0000", green: "00ff00" } # => { :red => "ff0000", ...}
+puts colors[:red]
+
+# range
+(1..5).include?(5) # true
+(1...5).include?(5) # false
+
+("a".."c").each { |s| puts s }
+
+pattern = /[0-9]+/
+p pattern === "HAL 9000"
+p pattern =~ "HAL 9000" # 4
+
+%r(/usr/bin) # regexp使うときに中で\が不要になって楽
+%r</usr/bin> # ()じゃなくてもOK
+
+# Proc
+greeter = Proc.new { |name|
+  puts "Hello, #{name}"
+}
+
+greeter.call "Proc"
+by_literal = ->(name) { puts "Hello, #{name}" } 
+
+# 代入
+a, *b = [1, 2, 3]
+c ||= 2
+
+# exception
+begin
+  1 / 0
+rescue => exception
+  puts exception
+end
+
+# ruby object2.rb foo
+p ARGV # => foo
