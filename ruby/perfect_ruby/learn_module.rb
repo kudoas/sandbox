3a47388d6572665aa1eef0e55e8b8c19f2e58583
daichi.kudo <daichi.kudo@classi.jp>
2022-12-26 23:36:47 +0900

    :sparkle: learn_moduleに移動

--- ruby/perfect_ruby/learn_module.rb
+++ ruby/perfect_ruby/learn_module.rb
@@ -1,3 +1,19 @@
+# インスタンス化できないクラスのようなもの
+# 名前空間として利用できる
+module Brainfsck
+  class Parser    
+  end
+end
+
+module Whitespace
+  class Parser
+  end
+end
+
+Brainfsck::Parser
+Whitespace::Parser
+
+
 module Sweet
   def self.lot
     %w[brownie apple-pie bavarois pudding].sample
