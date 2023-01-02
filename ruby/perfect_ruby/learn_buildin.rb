a2f2af9a3522b4b2090d1be7e8e43b9b5de36b6d
Daichi Kudo <sakanactor624@gmail.com>
2023-01-02 12:19:09 +0900

    :book: 138-141

--- ruby/perfect_ruby/learn_buildin.rb
+++ ruby/perfect_ruby/learn_buildin.rb
@@ -6,3 +6,22 @@ p 1 <=> 1
 ["Alice", "Bob", "Charlie"].sort do |a, b|
   a.length <=> b.length
 end
+
+3.step 10 do |num|
+  puts num
+end
+
+2.next
+2.succ
+
+(65..70).map { |n| p n.chr }
+Integer("2312")
+# Integer(nil) => TypeError
+
+1.upto 3 do |num|
+  puts num
+end
+
+arr = 20_230_102.digits
+p arr
+p 7.digits(2)
