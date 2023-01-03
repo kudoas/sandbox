1f6c5e08a616fabf066c07b403a58dc34ed9ec4f
Daichi Kudo <sakanactor624@gmail.com>
2023-01-03 23:25:57 +0900

    :book: 142-146

--- ruby/perfect_ruby/learn_buildin.rb
+++ ruby/perfect_ruby/learn_buildin.rb
@@ -25,3 +25,37 @@ end
 arr = 20_230_102.digits
 p arr
 p 7.digits(2)
+
+c = Complex(2, 3)
+c.real
+c.imaginary
+222.to_c # => (222+0i)
+
+"".empty?
+"てにをは".bytesize
+p "Alice Bod".include?("Bod")
+p "Highlight".start_with?("High")
+
+# 文字列はミュータブル
+str = "Pine"
+str << "apple"
+str # => "Pineapple"
+
+message = "The Answer to life, the unicerse, and everything: 42"
+message.slice(4)
+message.slice(4, 6)
+message.slice(4..9)
+message[4]
+
+# \t\r\n\f\vと空白
+spliter = " hi \t"
+spliter.strip
+spliter.rstrip
+
+"hi \n\n".chomp # => hi \n
+"Users".chop # => User
+"wooooooooooooooo".squeeze # => wo
+"aabbbcccdddd".squeeze("abc") # => abcdddd
+
+"Abc".swapcase # => aBC
+"title".capitalize # => Title
