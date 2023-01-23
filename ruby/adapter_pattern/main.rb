5bd14b1afe60ec6a7d2cc32dbb0eab80694128ef
Daichi Kudo <sakanactor624@gmail.com>
2023-01-23 23:37:35 +0900

    :pencil2: adapter

--- ruby/adapter_pattern/main.rb
+++ ruby/adapter_pattern/main.rb
@@ -12,3 +12,42 @@ class Printer
     @obj.print_strong
   end
 end
+
+# Targetにはないインターフェイスを持つ (Adaptee)
+class OldPrinter
+  def initialize(string)
+    @string = string.dup
+  end
+
+  # カッコに囲って文字列を表示する
+  def show_with_paren
+    puts "(#{@string})"
+  end
+
+  # アスタリスクで囲って文字列を表示する
+  def show_with_aster
+    puts "*#{@string}*"
+  end
+end
+# Targetが利用できるインターフェイスに変換 (Adapter)
+class Adapter
+  def initialize(string)
+    @old_printer = OldPrinter.new(string)
+  end
+
+  def print_weak
+    @old_printer.show_with_paren
+  end
+
+  def print_strong
+    @old_printer.show_with_aster
+  end
+end
+
+# 利用者(Client)
+p = Printer.new(Adapter.new("Hello"))
+p.print_weak
+#=> (Hello)
+
+p.print_strong
+#=> *Hello*
