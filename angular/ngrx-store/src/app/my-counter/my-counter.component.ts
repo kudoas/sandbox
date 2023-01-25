7cfb6786263cd5e2be27d67b21fd1d02de5ad0a0
Daichi Kudo <sakanactor624@gmail.com>
2023-01-25 23:29:54 +0900

    :books: draft my-counter

--- angular/ngrx-store/src/app/my-counter/my-counter.component.ts
+++ angular/ngrx-store/src/app/my-counter/my-counter.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-my-counter',
   templateUrl: './my-counter.component.html',
-  styleUrls: ['./my-counter.component.sass']
+  styleUrls: ['./my-counter.component.sass'],
 })
 export class MyCounterComponent {
+  count$: Observable<number>;
 
+  constructor() {
+    // TODO: Connect `this.count$` stream to the current store `count` state
+  }
+
+  increment() {
+    // TODO: Dispatch an increment action
+  }
+
+  decrement() {
+    // TODO: Dispatch a decrement action
+  }
+
+  reset() {
+    // TODO: Dispatch a reset action
+  }
 }
