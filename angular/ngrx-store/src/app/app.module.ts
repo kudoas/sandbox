8c3587ddc7065a667f8c1dcf1b0ef9865b718253
Daichi Kudo <sakanactor624@gmail.com>
2023-01-25 23:11:30 +0900

    :books: add my-counter

--- angular/ngrx-store/src/app/app.module.ts
+++ angular/ngrx-store/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MyCounterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ angular/ngrx-store/src/app/my-counter/my-counter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-counter',
+  templateUrl: './my-counter.component.html',
+  styleUrls: ['./my-counter.component.sass']
+})
+export class MyCounterComponent {
+
+}
