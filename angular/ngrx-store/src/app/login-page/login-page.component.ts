70605f3612e6dc35f2d76b3763f5d23c6b2bedb8
Daichi Kudo <sakanactor624@gmail.com>
2023-01-28 16:44:35 +0900

    :rocket: example using login-page action

--- angular/ngrx-store/src/app/login-page/login-page.component.ts
+++ angular/ngrx-store/src/app/login-page/login-page.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.sass']
+  styleUrls: ['./login-page.component.sass'],
 })
 export class LoginPageComponent {
-
+  // eg.
+  // onSubmit(username: string, password: string) {
+  //   store.dispatch(login({ username: username, password: password }));
+  // }
 }
