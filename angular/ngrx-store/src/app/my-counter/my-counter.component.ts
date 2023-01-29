0c3369c24052126201efe222cae7980e67006be5
Daichi Kudo <sakanactor624@gmail.com>
2023-01-29 16:59:01 +0900

    :rocket: move to actions directory

--- angular/ngrx-store/src/app/my-counter/my-counter.component.ts
+++ angular/ngrx-store/src/app/my-counter/my-counter.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { increment, decrement, reset } from '../counter.actions';
+import { increment, decrement, reset } from '../actions/counter.actions';
 
 @Component({
   selector: 'app-my-counter',
