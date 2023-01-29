4ee964e1e24035674c97350bafdd43d6bc197117
Daichi Kudo <sakanactor624@gmail.com>
2023-01-29 17:01:57 +0900

    :rocket: move to reducer directory

--- angular/ngrx-store/src/app/app.module.ts
+++ angular/ngrx-store/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter.reducer';
+import { counterReducer } from './reducer/counter.reducer';
 import { MyCounterComponent } from './my-counter/my-counter.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 
