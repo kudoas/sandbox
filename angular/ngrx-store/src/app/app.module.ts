abb6c908eb507bef9b7a34dabaf3f452a2b48337
Daichi Kudo <sakanactor624@gmail.com>
2023-01-29 18:03:36 +0900

    :rocket: resistor scoreboard reducer

--- angular/ngrx-store/src/app/app.module.ts
+++ angular/ngrx-store/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './reducer/counter.reducer';
 import { MyCounterComponent } from './my-counter/my-counter.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+
+import { counterReducer } from './reducer/counter.reducer';
+import { scoreboardReducer } from './reducer/scoreboard.reducer';
 
 @NgModule({
   declarations: [AppComponent, MyCounterComponent, LoginPageComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({ count: counterReducer }),
+    StoreModule.forRoot({ count: counterReducer, game: scoreboardReducer }),
   ],
   providers: [],
   bootstrap: [AppComponent],
