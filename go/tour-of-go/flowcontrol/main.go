1e6d9dedc61ad5a5de1268d2c8319e1f75a26129
Daichi Kudo <sakanactor624@gmail.com>
2023-02-06 23:20:42 +0900

    :book: flowcontrol 6-8

--- go/tour-of-go/flowcontrol/main.go
+++ go/tour-of-go/flowcontrol/main.go
@@ -12,6 +12,30 @@ func sqrt(x float64) string {
 	return fmt.Sprint(math.Sqrt(x))
 }
 
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
+}
+
+// Exercise: Loops and Functions
+func Sqrt(x float64) float64 {
+	z := float64(1)
+
+	for i := 0; i < 10; i++ {
+		diff := (z*z - x) / (2 * z)
+		if diff == 0 {
+			break
+		}
+		z -= diff
+	}
+
+	return z
+}
+
 func main() {
 	sum := 0
 	for i := 0; i < 10; i++ {
@@ -28,4 +52,11 @@ func main() {
 	// }
 
 	fmt.Println(sqrt(2), sqrt(-4))
+
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
+	fmt.Println(Sqrt(4))
 }
