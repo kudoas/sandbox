a12a9cf554f736d86b568a55a50fbabc8ae0cd17
Daichi Kudo <sakanactor624@gmail.com>
2023-03-18 20:19:29 +0900

    🎨 rename dist to public

--- front/webpack-sample/webpack.config.js
+++ front/webpack-sample/webpack.config.js
@@ -1,12 +1,15 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
+const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: {
     css: './assets/index.css'
   },
-  output: {},
+  output: {
+    path: path.resolve(__dirname, 'public')
+  },
   module: {
     rules: [
       {
