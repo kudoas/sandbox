48cdcbc9ffcb7b967526b4b627b359002f911bed
Daichi Kudo <sakanactor624@gmail.com>
2023-06-16 21:57:28 +0900

    :sparkles: define root token parser

--- paser/minute/src/parser.ts
+++ paser/minute/src/parser.ts
@@ -1,1 +1,10 @@
+import { Token } from "./models/token";
+
+const rootToken: Token = {
+  id: 0,
+  elmType: "root",
+  content: "",
+  parent: {} as Token,
+};
+
 export const parse = (markdownRow: string) => markdownRow;
