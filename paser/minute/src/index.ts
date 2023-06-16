096893e30b7833bbee0c51a09023795a2a5ec85d
Daichi Kudo <sakanactor624@gmail.com>
2023-06-16 21:50:59 +0900

    :sparkles: define convert to html string

--- paser/minute/src/index.ts
+++ paser/minute/src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello");
+import { parse } from "./parser";
+
+const convertToHTMLString = (markdown: string) => {
+  const mdArray = markdown.split(/\r\n|\r\n/);
+  const asts = mdArray.map((md) => parse(md));
+  return asts;
+};
