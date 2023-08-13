a465e40c1ecf24cd56b109324a97030d77f1d4b9
Daichi Kudo <sakanactor624@gmail.com>
2023-08-13 21:56:56 +0900

    TestSplitByLines

--- go/sample/split_test.go
+++ go/sample/split_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"os"
 	"testing"
 )
@@ -13,13 +14,13 @@ func TestSplitByBytes(t *testing.T) {
 		expectedError bool
 	}{
 		{
-			content:       "file: larger than bytesPerFile",
+			content:       "larger than bytesPerFile",
 			bytesPerFile:  10,
 			expectedFiles: []string{"1", "2", "3"},
 			expectedError: false,
 		},
 		{
-			content:       "file: smaller than bytesPerFile",
+			content:       "smaller than bytesPerFile",
 			bytesPerFile:  100000,
 			expectedFiles: []string{"1"},
 			expectedError: false,
@@ -59,6 +60,54 @@ func TestSplitByBytes(t *testing.T) {
 	}
 }
 
+func TestSplitByLines(t *testing.T) {
+	cases := []struct {
+		content       string
+		linesPerFile  int
+		expectedFiles []string
+		expectedError bool
+	}{
+		{
+			content:       "longer than linesPerFile \n rows",
+			linesPerFile:  1,
+			expectedFiles: []string{"1", "2"},
+			expectedError: false,
+		},
+		{
+			content:       "shorter than linesPerFile \n rows",
+			linesPerFile:  3,
+			expectedFiles: []string{"1"},
+			expectedError: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.content, func(t *testing.T) {
+			inputPath := "test_input.txt"
+			err := createTestFile(t, inputPath, c.content)
+			if err != nil {
+				t.Fatalf("Failed to create test input file: %v", err)
+			}
+			defer os.Remove(inputPath)
+
+			err = splitByLines(inputPath, c.linesPerFile)
+			if err != nil && !c.expectedError {
+				t.Fatalf("splitByLines returned an unexpected error: %v", err)
+			} else if err == nil && c.expectedError {
+				t.Fatalf("Expected error, but got none")
+			}
+			for _, expectedFile := range c.expectedFiles {
+				_, err := os.Stat(expectedFile)
+				if err != nil {
+					t.Errorf("File %s was not created as expected: %v", expectedFile, err)
+				}
+			}
+			verifyFileLine(t, c.linesPerFile, c.expectedFiles)
+			cleanTestFile(t, c.expectedFiles)
+		})
+	}
+}
+
 func createTestFile(t *testing.T, filePath, content string) error {
 	t.Helper()
 	file, err := os.Create(filePath)
@@ -80,7 +129,6 @@ func verifyFileSize(t *testing.T, expectedFileSize int, fileNames []string) {
 		file, err := os.Open(fileName)
 		if err != nil {
 			t.Errorf("Error opening file %s: %v", fileName, err)
-			continue
 		}
 		defer file.Close()
 
@@ -90,6 +138,27 @@ func verifyFileSize(t *testing.T, expectedFileSize int, fileNames []string) {
 		}
 		if expectedFileSize != 0 && int(info.Size()) > expectedFileSize {
 			t.Errorf("File %s size mismatch, Expected: %d, but got: %d", fileName, expectedFileSize, info.Size())
+		}
+	}
+}
+
+func verifyFileLine(t *testing.T, expectedLineCount int, fileNames []string) {
+	t.Helper()
+	for _, fileName := range fileNames {
+		file, err := os.Open(fileName)
+		if err != nil {
+			t.Errorf("Error opening file %s: %v", fileName, err)
+		}
+		scanner := bufio.NewScanner(file)
+		lineCount := 0
+		for scanner.Scan() {
+			lineCount++
+		}
+		if err := scanner.Err(); err != nil {
+			t.Fatalf("Error reading file %s: %v", fileName, err)
+		}
+		if int(lineCount) > expectedLineCount {
+			t.Errorf("File %s size mismatch, Expected: %d, but got: %d", fileName, expectedLineCount, lineCount)
 		}
 	}
 }
