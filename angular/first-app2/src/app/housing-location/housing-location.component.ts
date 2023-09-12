58bb4816e6e081f48216d5b61f6a6f99edfbb8fd
daichi.kudo <daichi.kudo@classi.jp>
2023-09-12 09:19:29 +0900

    tutorial(first-app): lessen3

---
+++ angular/first-app2/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <p>housing-location works!</p> `,
+  styleUrls: ['./housing-location.component.scss'],
+})
+export class HousingLocationComponent {}
