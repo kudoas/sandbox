eb1703991f9808697544ede58f69a960f1197d2a
Daichi Kudo <sakanactor624@gmail.com>
2023-10-09 18:29:41 +0900

    feat: Queryを変数で受け取れるかを試す

--- go/sync-issue/main.go
+++ go/sync-issue/main.go
@@ -14,16 +14,31 @@ func main() {
 		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
 	)
 	httpClient := oauth2.NewClient(context.Background(), src)
-
 	client := githubv4.NewClient(httpClient)
-	query := struct {
+
+	owner := "kudoas"
+	name := "sandbox"
+	issue := 151
+	variables := map[string]interface{}{
+		"repositoryOwner": githubv4.String(owner),
+		"repositoryName":  githubv4.String(name),
+		"issueNumber":     githubv4.Int(issue),
+	}
+	var query struct {
 		Repository struct {
-			DatabaseID githubv4.Int
-		} `graphql:"repository(owner: \"shurcooL\", name: \"notifications\")"`
-	}{}
-	err := client.Query(context.Background(), &query, nil)
+			Issue struct {
+				TrackedInIssues struct {
+					Nodes []struct {
+						ID githubv4.ID
+					}
+				} `graphql:"trackedInIssues(first: 5)"`
+			} `graphql:"issue(number: $issueNumber)"`
+		} `graphql:"repository(owner: $repositoryOwner, name: $repositoryName)"`
+	}
+
+	err := client.Query(context.Background(), &query, variables)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(query.Repository.DatabaseID)
+	fmt.Println(query.Repository.Issue.TrackedInIssues.Nodes[0].ID)
 }
