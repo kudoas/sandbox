6a1a19172684f30b712a113cc7b5de43f70766e0
Daichi Kudo <sakanactor624@gmail.com>
2023-10-09 22:34:44 +0900

    feat: 親 issue のメタ情報を子 issue に同期する

--- go/sync-issue/main.go
+++ go/sync-issue/main.go
@@ -27,6 +27,7 @@ func main() {
 	var query struct {
 		Repository struct {
 			Issue struct {
+				ID              githubv4.ID
 				TrackedInIssues struct {
 					Nodes []struct {
 						ID githubv4.ID
@@ -35,10 +36,60 @@ func main() {
 			} `graphql:"issue(number: $issueNumber)"`
 		} `graphql:"repository(owner: $repositoryOwner, name: $repositoryName)"`
 	}
-
 	err := client.Query(context.Background(), &query, variables)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(query.Repository.Issue.TrackedInIssues.Nodes[0].ID)
+	fmt.Println("children issue", query.Repository.Issue)
+
+	var query2 struct {
+		Node struct {
+			Issue struct {
+				Assignees struct {
+					Nodes []struct {
+						ID   githubv4.ID
+						Name githubv4.String
+					}
+				} `graphql:"assignees(first: 5)"`
+				Labels struct {
+					Nodes []struct {
+						ID   githubv4.ID
+						Name githubv4.String
+					}
+				} `graphql:"labels(first: 10)"`
+				Milestone struct {
+					ID    githubv4.ID
+					Title githubv4.String
+				} `graphql:"milestone"`
+			} `graphql:"... on Issue"`
+		} `graphql:"node(id: $issueID)"`
+	}
+	variables2 := map[string]interface{}{
+		"issueID": githubv4.ID(query.Repository.Issue.TrackedInIssues.Nodes[0].ID),
+	}
+	err = client.Query(context.Background(), &query2, variables2)
+	if err != nil {
+		println(fmt.Errorf("error: %v", err))
+	}
+
+	fmt.Println("issues", query2.Node.Issue)
+
+	var mutation struct {
+		UpdateIssue struct {
+			Issue struct {
+				ID githubv4.ID
+			}
+		} `graphql:"updateIssue(input: $input)"`
+	}
+
+	input := githubv4.UpdateIssueInput{
+		ID:          query.Repository.Issue.ID,
+		AssigneeIDs: &[]githubv4.ID{query2.Node.Issue.Assignees.Nodes[0].ID},
+		LabelIDs:    &[]githubv4.ID{query2.Node.Issue.Labels.Nodes[0].ID},
+		MilestoneID: &query2.Node.Issue.Milestone.ID,
+	}
+	err = client.Mutate(context.Background(), &mutation, input, nil)
+	if err != nil {
+		println(fmt.Errorf("error: %v", err))
+	}
 }
