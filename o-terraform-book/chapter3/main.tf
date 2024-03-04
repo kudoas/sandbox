d4662f6c3573b75b9c7e65448223cb4c98915b59
Daichi Kudo <sakanactor624@gmail.com>
2024-03-04 23:54:51 +0900

    動くところに戻す

--- o-terraform-book/chapter3/main.tf
+++ o-terraform-book/chapter3/main.tf
@@ -1,42 +1,45 @@
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "terraform-up-and-running-state-20240304"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-up-and-running-state-20240304"
 
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
-# resource "aws_s3_bucket_versioning" "enable" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+resource "aws_s3_bucket_versioning" "enable" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
-# resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-#   bucket = aws_s3_bucket.terraform_state.id
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       sse_algorithm = "AES256"
-#     }
-#   }
-# }
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
 
-# resource "aws_s3_bucket_public_access_block" "public_access" {
-#   bucket                  = aws_s3_bucket.terraform_state.id
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
-
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
-
-  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
+terraform {
+  backend "s3" {
+    bucket  = "terraform-up-and-running-state-20240304"
+    key     = "global/s3/terraform.tfstate"
+    region  = "us-west-2"
+    encrypt = true
+  }
 }
+
