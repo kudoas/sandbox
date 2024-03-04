3321543bc4e26aebc2f231d9bcf23e742ac4fdaf
Daichi Kudo <sakanactor624@gmail.com>
2024-03-04 23:34:19 +0900

    terraform book(p96): workspace の migrate がうまくいかなくて一旦push

--- o-terraform-book/chapter3/main.tf
+++ o-terraform-book/chapter3/main.tf
@@ -1,43 +1,42 @@
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "terraform-up-and-running-state-20240304"
+
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+# }
+
+# resource "aws_s3_bucket_versioning" "enable" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+#   bucket = aws_s3_bucket.terraform_state.id
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
+
+# resource "aws_s3_bucket_public_access_block" "public_access" {
+#   bucket                  = aws_s3_bucket.terraform_state.id
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-2"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-up-and-running-state-20240304"
+resource "aws_instance" "example" {
+  ami = "ami-0fb653ca2d3203ac1"
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  instance_type = terraform.workspace == "default" ? "t2.medium" : "t2.micro"
 }
-
-resource "aws_s3_bucket_versioning" "enable" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
-  bucket = aws_s3_bucket.terraform_state.id
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket                  = aws_s3_bucket.terraform_state.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-terraform {
-  backend "s3" {
-    # 残りの設定は backend.hcl に移動
-    key = "global/s3/terraform.tfstate"
-  }
-}
-
