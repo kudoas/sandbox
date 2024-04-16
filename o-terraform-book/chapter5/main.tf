22365c367bba2df1086f9ddae16f94d659716473
Daichi Kudo <sakanactor624@gmail.com>
2024-04-16 22:08:10 +0900

    wip

--- o-terraform-book/chapter5/main.tf
+++ o-terraform-book/chapter5/main.tf
@@ -45,3 +45,30 @@ variable "give_neo_cloudwatch_full_access" {
   description = "If true, neo gets full access to CloudWatch"
   type        = bool
 }
+
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full_access" {
+  count      = var.give_neo_cloudwatch_full_access ? 1 : 0
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_full_access.arn
+}
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only" {
+  count      = var.give_neo_cloudwatch_full_access ? 0 : 1
+  user       = aws_iam_user.example[0].name
+  policy_arn = aws_iam_policy.cloudwatch_read_only.arn
+}
+
+# output "neo_cloudwatch_policy_arn" {
+#   value = (
+#     var.give_neo_cloudwatch_full_access
+#     ? aws_iam_user_policy_attachment.neo_cloudwatch_full_access[0].policy_arn
+#     : aws_iam_user_policy_attachment.neo_cloudwatch_read_only[0].policy_arn
+#   )
+# }
+
+output "neo_cloudwatch_policy_arn" {
+  value = one(concat(
+    aws_iam_user_policy_attachment.neo_cloudwatch_full_access[*].policy_arn,
+    aws_iam_user_policy_attachment.neo_cloudwatch_read_only[*].policy_arn
+  ))
+}
