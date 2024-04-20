e5135f982f2da2afc6fac6fece3e754400eb7ad7
Daichi Kudo <sakanactor624@gmail.com>
2024-04-20 20:37:38 +0900

    sample github v4

--- go/generate-github-app-token/main.go
+++ go/generate-github-app-token/main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
 
 func main() {
-	// This is a placeholder for the main function.
-	fmt.Println("Hello, playground")
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	client := githubv4.NewClient(httpClient)
+
+	var query struct {
+		Viewer struct {
+			Login     githubv4.String
+			CreatedAt githubv4.DateTime
+		}
+	}
+
+	err := client.Query(context.Background(), &query, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("Login:", query.Viewer.Login)
+	fmt.Println("CreatedAt:", query.Viewer.CreatedAt)
 }
