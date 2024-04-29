be3a2ae8dea92091ae4a9daee3836c4736a1a89c
Daichi Kudo <sakanactor624@gmail.com>
2024-04-29 23:17:46 +0900

    book(Goで学ぶGraphQLサーバーサイド入門): Chapter 02
    * https://zenn.dev/hsaki/books/golang-graphql/viewer/tutorial

--- graphql/graph/schema.resolvers.go
+++ graphql/graph/schema.resolvers.go
@@ -6,18 +6,43 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"my_gql_server/graph/model"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	return &model.Todo{
+		ID:   "T1000",
+		Text: input.Text,
+		User: &model.User{
+			ID:   input.UserID,
+			Name: "user1",
+		},
+	}, nil
 }
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return []*model.Todo{
+		{
+			ID:   "TODO-1",
+			Text: "My Todo 1",
+			User: &model.User{
+				ID:   "User-1",
+				Name: "hsaki",
+			},
+			Done: true,
+		},
+		{
+			ID:   "TODO-2",
+			Text: "My Todo 2",
+			User: &model.User{
+				ID:   "User-1",
+				Name: "hsaki",
+			},
+			Done: false,
+		},
+	}, nil
 }
 
 // Mutation returns MutationResolver implementation.
