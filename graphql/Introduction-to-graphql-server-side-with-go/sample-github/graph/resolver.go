7e999d856a2ff5fad793f108478b32c9e3bff79e
Daichi Kudo <sakanactor624@gmail.com>
2024-05-09 00:50:30 +0900

    impl user resolver

--- graphql/Introduction-to-graphql-server-side-with-go/sample-github/graph/resolver.go
+++ graphql/Introduction-to-graphql-server-side-with-go/sample-github/graph/resolver.go
@@ -1,7 +1,11 @@
 package graph
 
+import "github.com/kudoas/graphql-sample/graph/services"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	Srv services.Services
+}
