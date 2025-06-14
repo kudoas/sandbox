8541e0f30d7946961f76f590464e7fbe63a28f8d
Daichi Kudo <sakanactor624@gmail.com>
2025-06-14 10:21:43 +0900

    steep check

--- rails/enjoy-rbs/person.rb
+++ rails/enjoy-rbs/person.rb
@@ -1,16 +1,34 @@
-class Person
-  attr_reader :name
-  attr_reader :contacts
+# rbs_inline: enabled
 
-  def initialize(name:)
+class Person
+  attr_reader :name #: String
+
+  attr_reader :addresses #: Array[String]
+
+  # You can write the type of parameters and return types.
+  #
+  # @rbs name: String
+  # @rbs addresses: Array[String]
+  # @rbs return: void
+  def initialize(name:, addresses:)
     @name = name
-    @contacts = []
+    @addresses = addresses
   end
 
-  def speak
-    "I'm #{@name} and I love Ruby!"
+  # Or write the type of the method just after `@rbs` keyword.
+  #
+  # @rbs () -> String
+  def to_s
+    "Person(name = #{name}, addresses = #{addresses.join(", ")})"
+  end
+
+  # @rbs &block: (String) -> void
+  def each_address(&block) #: void
+    addresses.each(&block)
   end
 end
 
-person = Person.new(name: "John")
-person.speak
+person = Person.new(name: "Alice", addresses: ["123 Main St", "456 Elm St"])
+person.each_address do |address|
+  puts "Address: #{address}"
+end
